c30511ee420b0c277149897b4830de8d932f1d74
HirokiYamamoto <b08109@gmail.com>
2015-12-13 01:53:17 +0900

    地図を軽く実装

---
+++ safe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  safe
+//
+//  Created by 山本洸希 on 2015/12/12.
+//  Copyright © 2015年 keito5656. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ safeTests/safeTests.m
@@ -0,0 +1,39 @@
+//
+//  safeTests.m
+//  safeTests
+//
+//  Created by 山本洸希 on 2015/12/12.
+//  Copyright © 2015年 keito5656. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface safeTests : XCTestCase
+
+@end
+
+@implementation safeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
